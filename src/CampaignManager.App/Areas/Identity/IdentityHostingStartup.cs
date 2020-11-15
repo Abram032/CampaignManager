0eeb5c2651ab7dc4a7118f19a7c8dc9b8b97ab2a
Adam Błaszczyk <adam.blaszczyk@outlook.in>
2020-11-15 08:26:04 +0100

    Added project files for react app

---
+++ src/CampaignManager.App/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using CampaignManager.App.Data;
+using CampaignManager.App.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CampaignManager.App.Areas.Identity.IdentityHostingStartup))]
+namespace CampaignManager.App.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
