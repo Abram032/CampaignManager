992b7bb74e57d7f0991f99dcaf81c1ccde995dab
Adam Błaszczyk <adam.blaszczyk@outlook.in>
2020-12-19 23:12:16 +0100

    Added database auto migration

--- src/CampaignManager.App/Program.cs
+++ src/CampaignManager.App/Program.cs
@@ -2,8 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CampaignManager.App.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +18,9 @@ namespace CampaignManager.App
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            PrepareDatabaseAsync(host).Wait();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +29,39 @@ namespace CampaignManager.App
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static async Task PrepareDatabaseAsync(IHost host)
+        {
+            using(var scope = host.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var userDbContext = scope.ServiceProvider.GetService<UserDbContext>();
+                if(await userDbContext.Database.GetService<IRelationalDatabaseCreator>().ExistsAsync() == false)
+                {
+                    await userDbContext.Database.MigrateAsync();
+                }
+                else
+                {
+                    var migrations = await userDbContext.Database.GetPendingMigrationsAsync();
+                    if(migrations.ToList().Count > 0)
+                    {
+                        await userDbContext.Database.MigrateAsync();
+                    }
+                }
+
+                var appDbContext = scope.ServiceProvider.GetService<AppDbContext>();
+                if(await appDbContext.Database.GetService<IRelationalDatabaseCreator>().ExistsAsync() == false)
+                {
+                    await appDbContext.Database.MigrateAsync();
+                }
+                else
+                {
+                    var migrations = await appDbContext.Database.GetPendingMigrationsAsync();
+                    if(migrations.ToList().Count > 0)
+                    {
+                        await appDbContext.Database.MigrateAsync();
+                    }
+                }
+            }
+        }
     }
 }
